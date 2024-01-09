360ceaff35c98c9cf80bc195797f7ad0e1d8c9f0
Jonas Hessling <firstname.jonas@icloud.com>
2024-01-09 13:12:33 +0000

    Add menu for game setup

--- run.py
+++ run.py
@@ -5,4 +5,26 @@ def welcome():
     print('Welcome to the text-based adventure game "QB1".')
     print("In this game you are the quarterback of the New York Giants in the Super Bowl against the Kansas City Chiefs.")
     print("Make the right decisions and win the game of the year!\n")
-    #Add User Choice function in menu: show rules / start game here!
+    user_choice_welcome()
+
+"""
+Function to let user decide between starting the game or reading the rules
+"""
+def user_choice_welcome():
+    print("Do you want to start the game or read the rules?")
+    print("1. Start the game")
+    print("2. Read the rules\n")
+
+    user_choice_welcome_input = input("Your choice (1/2): ")
+
+    if user_choice_welcome_input == "1":
+        print("Let the games begin...")
+        #Add function for game start here!
+    elif user_choice_welcome_input == "2":
+        print("Here are the rules!")
+        #Add function for game start here!
+    else:
+        print("Invalid input. Please choose again.")
+        return user_choice_welcome()
+
+welcome()
