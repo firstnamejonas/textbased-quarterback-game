c47f8cbc908ab327e8c55c74800f4bb203bacd8e
Jonas Hessling <firstname.jonas@icloud.com>
2024-01-09 13:04:48 +0000

    Add function to display game menu

---
+++ run.py
@@ -0,0 +1,8 @@
+"""
+Start of the game function
+"""
+def welcome():
+    print('Welcome to the text-based adventure game "QB1".')
+    print("In this game you are the quarterback of the New York Giants in the Super Bowl against the Kansas City Chiefs.")
+    print("Make the right decisions and win the game of the year!\n")
+    #Add User Choice function in menu: show rules / start game here!
